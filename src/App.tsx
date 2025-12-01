6a39c24ad2df6bf10cf5a07527cb69c5f03fa4fa
Juan122113 <ammiel89@hotmail.com>
2025-12-01 14:44:51 -0300

    Add advice fetching logic and attribution styling
    Implemented fetching advice from the Advice Slip API using useEffect and async/await in App.tsx. Added state management for advice data, loading, and error handling. Also added a CSS rule for attribution link color in App.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,67 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [adviceId, setAdviceId] = useState();
+  const [advice, setAdvice] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // fetch("https://api.adviceslip.com/advice")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setData(data);
+    //     setLoading(false);
+    //   })
+    //   .catch((err) => {
+    //     setError(err);
+    //     setLoading(false);
+    //   });
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://api.adviceslip.com/advice");
+
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        setData(data);
+        console.log(data);
+        console.log(data.advice);
+        console.log(data[advice]);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className='text-[50px]'>
-      Hello World
+    <div>
+      <div>
+
+        <p>Advice {adviceId}</p>
+
+        <p>"{advice}"</p>
+
+        <img src="./images/pattern-divider-mobile.svg" alt="pattern divider" />
+
+        <img src="./images/icon-dice.svg" alt="icon dice" />
+
+        <div className="attribution text-[11px] text-center">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="#">Your Name  Here</a>.
+        </div>
+      </div>
     </div>
+    
   )
 }
 
