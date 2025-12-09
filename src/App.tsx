ca13d95f10b5e8c95004a830b9f6bc186bf3b5c1
Juan122113 <ammiel89@hotmail.com>
2025-12-09 13:34:32 -0300

    Refine layout and styles for advice card component
    Improved responsive design and styling for the advice card in App.tsx and index.css. Added new class names for better targeting, adjusted margin and sizing for larger screens, and enhanced hover effects for the button container.

--- src/App.tsx
+++ src/App.tsx
@@ -37,26 +37,26 @@ function App() {
   }, []);
 
   return (
-    <main className="min-h-screen bg-[#1f2632] flex flex-col items-center relative">
-      <div className='card h-[19.7rem] bg-[#323a49] flex flex-col items-center justify-center mt-[7.5rem] w-[21.4rem] rounded relative max-w-[95%] lg:mt-[15rem] md:mt-15rem md:bg-red-500 lg:bg-red-500'>
+    <main className="min-h-screen bg-[#1f2632] flex flex-col items-center relative lg:justify-center">
+      <div className='card h-[19.7rem] bg-[#323a49] flex flex-col items-center justify-center mt-[7.5rem] w-[21.4rem] rounded relative max-w-[95%] lg:mt-[200px] md:mt-15rem md:bg-red-500 lg:bg-red-500'>
 
         {loading ? (
           // <div className="text-[#cee3e9] text-center ">
-            <p className='text-[#cee3e9] text-[1.55rem] font-[800] '>Thinking...</p>
+            <p className='text-[#cee3e9] text-[1.55rem] font-[800] text.center'>Thinking...</p>
             
           
         ) : error ? (
           <p>{error}</p>
         ) : (
           <>
-            <p className='text-[#00FFA0] text-[.64rem] tracking-[.23rem] text-center font-[500]'>ADVICE #{adviceId}</p>
-            <p className='text-[#cee3e9] text-[1.55rem]  text-center relative bottom-[10px] max-w-[90%] font-[800]'>"{advice}"</p>
+            <p className='advice-id text-[#00FFA0] text-[.64rem] tracking-[.23rem] text-center font-[500] lg:mt-[2rem]'>ADVICE #{adviceId}</p>
+            <p className='advice text-[#cee3e9] text-[1.55rem]  text-center relative bottom-[10px] max-w-[90%] font-[800]'>"{advice}"</p>
           </>
         )}
         
-        <img src="./images/pattern-divider-mobile.svg" alt="pattern divider" className='relative bottom-[15px] ' />
+        <img src="./images/pattern-divider-mobile.svg" alt="pattern divider" className='pattern-divider relative bottom-[15px] ' />
 
-        <div onClick={fetchData} className='rounded-[50%] bg-[#00FFA0] absolute p-[1.25rem] flex top-[283px] lg:relative'>
+        <div onClick={fetchData} className='button-container rounded-[50%] bg-[#00FFA0] absolute p-[1.25rem] flex top-[283px] cursor-pointer '>
           <button type='button' className='bg-[url("./images/icon-dice.svg")] w-[1.5rem] h-[1.5rem] cursor-pointer bg-[#00FFA0] bg-contain bg-no-repeat border-none m-auto' disabled={loading}></button>
         </div>
 
